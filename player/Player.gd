6fb60539981c4872b5a4bd0722fcd1209cc58201
Ambrose Hlustik-Smith <a.hlustiksmith@gmail.com>
2019-08-13 13:48:24 +0100

    Added a test level

--- player/Player.gd
+++ player/Player.gd
@@ -7,20 +7,21 @@ export var jump_speed = -400
 var velocity = Vector2()
 
 func get_input():
-    velocity.x = 0
+	velocity.x = 0
 	
-    var right = Input.is_action_pressed('right')
-    var left = Input.is_action_pressed('left')
-    var jump = Input.is_action_just_pressed('jump')
+	var right = Input.is_action_pressed('right')
+	var left = Input.is_action_pressed('left')
+	var jump = Input.is_action_just_pressed('jump')
 
-    if is_on_floor() and jump:
-        velocity.y = jump_speed
-    if right:
-        velocity.x += run_speed
-    if left:
-        velocity.x -= run_speed
+	if is_on_floor() and jump:
+		velocity.y = jump_speed
+	if right:
+		velocity.x += run_speed
+	if left:
+		velocity.x -= run_speed
 
 func _physics_process(delta):
-    velocity.y += gravity * delta
-    get_input()
-    velocity = move_and_slide(velocity, Vector2(0, -1))
+	print(OS.get_window_size())
+	velocity.y += gravity * delta
+	get_input()
+	velocity = move_and_slide(velocity, Vector2(0, -1))
